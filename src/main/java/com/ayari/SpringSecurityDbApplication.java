d521ab5f79a4914e42b591fea5d47e205147dcf8
ayari nadhmi <nadhmi.ayari@esprit.tn>
2019-05-05 21:33:58 +0100

    first commit

---
+++ src/main/java/com/ayari/SpringSecurityDbApplication.java
@@ -0,0 +1,13 @@
+package com.ayari;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityDbApplication.class, args);
+	}
+
+}
